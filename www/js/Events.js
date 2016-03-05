50f80541fdb4ed4a9d2b16514db3c61f609c5b49
lramo062 <lramo062@fiu.edu>
2016-03-05 17:28:10 -0500

    events js boss

---
+++ www/js/Events.js
@@ -0,0 +1,63 @@
+angular.module('starter.controllers').factory('MiamiEvents', function() {
+  var theUrl = 'http://www.miamibeachapi.com/rest/a.pi/events/search';
+	var consumerKey = "anonymous";
+	var consumerSecret = "anonymous";
+	var token = "5f02d1155718dd804e34a094c19d408d0ae585d7";
+	var tokenSecret = "e35439d74db66d1367f69d2cf84c5032ae2af960";
+
+	var accessor = {
+		consumerKey: consumerKey
+		, consumerSecret : consumerSecret
+		, token  : token
+		, tokenSecret   : tokenSecret
+	};
+
+	var message = {
+		method: "GET"
+		, action: theUrl
+		, parameters: {}
+	};
+
+  var list = [];
+
+  return{
+    getData: function(filter) {
+        message.parameters = {};
+    		message.parameters['rows'] = 100;
+    		message.parameters['page'] = 1;
+    		message.parameters['category_filter'] = filter;
+    		message.parameters['jsoncallback'] = '?';
+    		message.parameters['oauth_timestamp'] = freshTimestamp();
+    		message.parameters['oauth_nonce'] = freshNonce();
+
+        OAuth.completeRequest(message, accessor);
+		    OAuth.SignatureMethod.sign(message, accessor);
+		    var encodedParams = OAuth.formEncode(message.parameters);
+		    var getURL = theUrl + '?' + encodedParams;
+
+        $.getJSON(getURL, function(data){
+    			var outHTML = "";
+    			if(data.events){
+            list = data;
+            console.log(list['events']);
+    			}
+    			$('#results').html(outHTML);
+    		});
+
+  },
+
+  getList : function() {
+    console.log(list);
+    return list['events'];
+  }
+}
+
+  function freshTimestamp() {
+		return OAuth.timestamp();
+	};
+	function freshNonce() {
+		return OAuth.nonce(11);
+	};
+
+})
+
